4e8ed73a715998c58a0717726c451a8270156ccd
Noam Amit <noamamit92@gmail.com>
2017-02-12 13:20:20 +0200

    hide scroll mask to make full view elements available after closing the dialog

--- js/primo-explore-cam-locations-after.js
+++ js/primo-explore-cam-locations-after.js
@@ -161,6 +161,10 @@ class LocationItemsBeforeController {
                     controller: function controller($scope, $mdDialog) {
 
                         $scope.closeDialog = function () {
+                            let mask = angular.element(document.getElementsByClassName('md-scroll-mask'));
+                            if(mask && mask.length > 0){
+                                angular.element(document.getElementsByClassName('md-scroll-mask'))[0].remove();
+                            }
                             $mdDialog.hide();
                         };
                     }
